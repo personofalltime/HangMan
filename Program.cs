adef0f9cbc3663ec3a2434305cd3b16d7d1c0120
personofalltime <66888981+personofalltime@users.noreply.github.com>
2023-09-20 14:42:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DPT_200923
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Gallows
+    {
+
+    }
+}
